322fe4db43937d0a5071eb869affd9adf468fbe0
Giménez Silva Germén Alberto <ggerman@gmail.com>
2026-01-07 11:54:27 -0300

    update library

--- lib/gd/gis/layer_points.rb
+++ lib/gd/gis/layer_points.rb
@@ -1,14 +1,22 @@
 module GD
   module GIS
     class PointsLayer
-      def initialize(data, lon:, lat:, icon:)
+      def initialize(data, lon:, lat:, icon:, label: nil, font: nil, size: 12, color: [0,0,0])
         @data = data
         @lon = lon
         @lat = lat
         @icon = GD::Image.open(icon)
+
+        @label = label
+        @font  = font
+        @size  = size
+        @color = color
+
+        @icon.alpha_blending = true
+        @icon.save_alpha = true
       end
 
-      def render!(img, projection)
+      def render!(img, projector)
         w = @icon.width
         h = @icon.height
 
@@ -16,8 +24,26 @@ module GD
           lon = @lon.call(row)
           lat = @lat.call(row)
 
-          x,y = projection.call(lon,lat)
+          x,y = projector.call(lon,lat)
+
+          # icono
           img.copy(@icon, x - w/2, y - h/2, 0,0,w,h)
+
+          # etiqueta opcional
+          if @label && @font
+            text = @label.call(row)
+            unless text.nil? || text.strip.empty?
+              font_h = @size * 1.1
+
+              img.text(text,
+                x: x + w/2 + 4,
+                y: y + font_h/2,
+                size: @size,
+                color: @color,
+                font: @font
+              )
+            end
+          end
         end
       end
     end
